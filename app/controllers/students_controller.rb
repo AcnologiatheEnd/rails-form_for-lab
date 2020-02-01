476089fdd417a32d1b5bceb679a99959afbe21ad
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-01 09:15:04 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,8 @@ class StudentsController < ApplicationController
   end
   
   def create
-    
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
   
   def edit 
@@ -17,6 +18,17 @@ class StudentsController < ApplicationController
   end
   
   def show 
-    
+    @student = Student.find(params[:id])
   end 
+  
+  private
+  
+    # We pass the permitted fields in as *args;
+    # this keeps `post_params` pretty dry while
+    # still allowing slightly different behavior
+    # depending on the controller action. This
+    # should come after the other methods
+	  def student_params(*args)
+	    params.require(:student).permit(*args)
+	  end
 end
