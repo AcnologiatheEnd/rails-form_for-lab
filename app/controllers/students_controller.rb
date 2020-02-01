63449818caf9d65cb8746bcb6203597afc5ad33d
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-01 09:02:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,22 @@
 class StudentsController < ApplicationController
   
   def new 
+    @student = Student.new 
+  end
+  
+  def create
     
   end
+  
+  def edit 
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show 
+    
+  end 
 end
