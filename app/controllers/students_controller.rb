82a7b23f0addb7617d0096d8700b0c89e691dc7c
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-01 09:18:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,11 +10,12 @@ class StudentsController < ApplicationController
   end
   
   def edit 
-    
+    @student = Student.find(params[:id])
   end
   
   def update
-    
+    @student = Student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
   
   def show 
